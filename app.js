18ff820c66234d4210e47c18c6341c409ca75efa
JeanOjeda-Dev <jeanojeda2504@gmail.com>
2026-07-24 11:49:48 -0500

    configuracion de variables parte 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express'; // Método ES Modules
 
 const app = express();
-const port = 3000;
+const port =process.env.PUERTO|| 3000
 
 app.get('/', (req, res) => {
 res.send('Aprendiendo express, ya me quiero ir a la houseee');
