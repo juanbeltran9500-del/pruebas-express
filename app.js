2ed9dad3df6f57cfe48e73023fecf1a728738be6
JeanOjeda-Dev <jeanojeda2504@gmail.com>
2026-07-24 11:41:52 -0500

    configuracion de entorno virtual

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-res.send('Aprendiendo express, kkakakkakakakakkaka');
+res.send('Aprendiendo express, ya me quiero ir a la houseee');
 });             
 
 app.listen(port, () => {
