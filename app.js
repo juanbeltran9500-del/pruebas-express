c4f215a13c8bf19ea5375e7429c7523b746109bf
juanbeltran9500-del <juanbeltran9500@gmail.com>
2026-07-31 16:09:10 -0500

    prueba

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ import express from 'express'; // Método ES Modules
 import {configDotenv} from "dotenv"
 configDotenv()
 const app = express();
-const port =process.env.PUERTO|| 3000
+const port =process.env.PUERTO|| 5050;
 app.get('/', (req, res) => {
 res.send('Aprendiendo express, ya me quiero ir a la houseee,ADSO EN EL SENA 31 de julio');
 });      
-//ot       
-app.listen(port, () => {
-console.log('Servidor en funcionamiento en el puerto: ' + port);
-});
+//otro endpoint
+app.get("/otra ruta", (req,res)=>{
+    //uswando template string
+    res.send(`<h1>otra ejemplo de ruta</h1>h2>End point con res.send</h2>`)
+})
+
+app.get("ruta2",(req,res)=>{
+    res.json({"nombre": "juan", "apellido": "beltran","cargo":
+        "aprendiz"})
+
+    })
+    
+    
+app.listen(port,function(){
+    console.log(`Servidor: http://localhost:${port}`)
+})
 
 
