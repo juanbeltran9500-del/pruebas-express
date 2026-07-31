ab89566e6649fa8eab3210aa53b78b2a6673e385
SnickerRay <ikrromm368@gmail.com>
2026-07-31 15:25:02 -0500

    configurando variables de entorno

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from 'express'; // Método ES Modules
-
+//leer el archivo .envi
+import {configDotenv} from "dotenv"
+configDotenv()
 const app = express();
 const port =process.env.PUERTO|| 3000
-
 app.get('/', (req, res) => {
-res.send('Aprendiendo express, ya me quiero ir a la houseee');
-});             
-
+res.send('Aprendiendo express, ya me quiero ir a la houseee,ADSO EN EL SENA 31 de julio');
+});      
+//ot       
 app.listen(port, () => {
 console.log('Servidor en funcionamiento en el puerto: ' + port);
 });
+
+
