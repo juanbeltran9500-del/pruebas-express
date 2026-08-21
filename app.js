ca34b57267b6b6c7c263f964fade51c1ecb76989
Esteban Beltran <juanbeltran9500@gmail.com>
2026-08-21 10:52:53 -0500

    actualizacion de descripcion

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
+require("dotenv/config")
+const puerto = process.env.PUERTO || 3000
 
 app.get("/", (_, res) => {
   res.send(`API Rest`, );
 });
 
 app.listen(3000, () => {
-  console.log(`Servidor en funcionamiento en el puerto: http://localhost:3000`);
+  console.log(`Servidor en funcionamiento en el puerto: http://localhost:${puerto}`);
 });
