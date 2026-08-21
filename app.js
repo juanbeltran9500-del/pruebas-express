95652f859308f20b69c199f5b922e8ec2ce964f5
Esteban Beltran <juanbeltran9500@gmail.com>
2026-08-21 11:40:48 -0500

    actualizacion de endpoint

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const express = require("express")
-const app = express()
+const express = require('express'); 
+const app = express();  
 require("dotenv/config")
-const puerto = process.env.PUERTO || 3000
+const puerto = process.env.PORT|| 3030
+const sistemaArchivo = require("fs")
+const ruta = require("path")
+const rutaArchivoJson = ruta.join(__dirname, "datos.json")
+
 
 app.get("/", (_, res) => {
-  res.send(`API Rest`, );
+    res.send("Hola aprendiendo express");
 });
 
-app.listen(3000, () => {
-  console.log(`Servidor en funcionamiento en el puerto: http://localhost:${puerto}`);
-});
+app.get("/api/aprendiz", (req, res)=>{
+    sistemaArchivo.readFile(rutaArchivoJson, "utf-8", (error, datos)=>{
+        if(error){
+            return res.json({Error:"no se puede leer los datos."})
+        }
+        const listaAprendices = JSON.parse(datos)
+        res.json(listaAprendices)
+    })
+})
+
+app.listen(puerto, function () {
+     console.log( `SERVIDOR http://localhost:${puerto}`) 
+}); 
