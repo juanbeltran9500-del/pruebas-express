f714fb492da5793de2fe01e7efae1408fd7d3fa5
Esteban Beltran <juanbeltran9500@gmail.com>
2026-08-28 11:37:43 -0500

    endpoin y formularios creacion

--- app.js
+++ app.js
@@ -1,47 +1,61 @@
-import  express  from "express";
-
+//const exoress = require("express")
+import express from 'express';
 //leer el archivo .env
 import {configDotenv} from "dotenv"
 configDotenv()
 
-const app = express();
-const port = process.env.PUERTO || 5050;
 
+const app = express();  
+const port = process.env.PUERTO || 3030; 
+
+
+//uso de middleware body-parse
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-app.get("/", (_, res) => {
-    res.send('Aprendiendo Express,ficha 3407181, ADSO EN EL SENA 31 de julio');
-});
+app.get("/", (req, res) => {
+    res.send("Aprendiendo express, con la ficha 3407181, ADSO en el SENA 31 de julio ")
+})
+
+//otro endpoint 
+app.get("/otraruta", (req, res)=>{
+    //usando template string
+    res.send(`<h1>Otro ejemplo de ruta</h1>
+        <h2>End point con res.send</h2>`)
+})
+
 
 app.get("/ruta2", (req, res)=>{
-    //usando template string
-    res.json({"nombre":"Santiago", "apellido":"Martinez", "ficha":"3407181", "cargo":"aprendiz"})
+    res.json({"Nombre": "esteban", "Apellido": "Beltrán", "Cargo": "Aprendiz"})
 })
 
-app.get("/ruta3/:aprendiz/:otro_dato", (req, res) =>{
+app.get("/ruta3/:aprendiz/:otrodato", (req, res)=>{
     const dato_aprendiz = req.params.aprendiz
-    const otro_dato = req.params.aprendiz
-    res.json ({"nombre": dato_aprendiz, "otro": otro_dato})
+    const otro_dato = req.params.otrodato
+    res.json({"Nombre": dato_aprendiz, "Otro": otro_dato})
 })
 
-app.get("/ruta4", (req, res) =>{
+
+app.get("/ruta4", (req, res)=>{
+    //capturar el parametro de consulta query
     const orden = req.query.orden || "sin ordenar"
-    const pagina = req.query.pagina
-    res.send(`<h1>Listado de aprendices</h1>
-        <p>el listado en orden ${orden}</p>
-        <p>pagina: ${pagina}</p>
-        `)
+    const pagina = req.query.pagina || 1
+    res.send(`<h1>Listado Aprendices</h1>
+        <p>El listado esta en orden ${orden}</p>
+        <p>Pagina; ${pagina} </p>`)
 })
 
-app.post("/ruta2", (req, res) =>{
+//enpoint para envio de datos formato JSON
+app.post("/ruta2",(req, res)=>{
     const todosDatos = req.body
     const name = req.body.nombre
     const lastname = req.body.cargo
-    res.status(201).json({Datos: todosDatos,nombre:name,
-        cargo: lastname})
-    })
+    res.status(201).json({Datos: todosDatos, nombre: name, cargo: lastname})
 
-    app.post("/login", (req, res) => {
+})
+
+
+ app.post("/login", (req, res) => {
 
     const usuario = req.body.usuario;
     const contraseña = req.body.contraseña;
@@ -70,6 +84,18 @@ app.post("/ruta2", (req, res) =>{
 
 });
 
-app.listen(port, function(){
-console.log( `SERVIDOR: http://localhost:${port}`);
-});
+app.post("/formulario",  (req,res)=>{
+    const datosForm = req.body
+    const miNombre = req.body.nombre1
+    const miApellido = req.body.apellido2
+    const miCargo = req.body.cargo3
+    res.status(200).json({Mensaje: "Datos recibidos", nombre: miNombre, apellido: miApellido, cargo: miCargo})
+})
+
+
+
+
+
+app.listen(port, () => {
+     console.log( `Servidor en funcionamiento en el puerto: ${port} http://localhost:3030`); 
+}); 
