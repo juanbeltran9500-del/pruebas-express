936f78c7b708ace07a010b8dc74a5c359e28ca73
Juan-Beleno <Juanchobele16@gmail.com>
2026-09-11 08:44:51 -0500

    endpoint de crear aprendiz con form y json utilizando middleware body parse

--- app.js
+++ app.js
@@ -1,29 +1,53 @@
-const express = require('express'); 
-const app = express();  
+const express = require("express")
+const app = express() 
 require("dotenv/config")
-const puerto = process.env.PORT|| 3030
+const port = process.env.puerto
+const PUERTO = port || 3030
 const sistemaArchivo = require("fs")
-const ruta = require("path")
+const ruta= require("path")
 const rutaArchivoJson = ruta.join(__dirname, "datos.json")
+//importar libreria para subir archivos 
+const multer = require("multer")
 
-
-app.get("/", (_, res) => {
-    res.send("Hola aprendiendo express");
-});
-
-app.get("/api/aprendiz", (req, res)=>{
-    sistemaArchivo.readFile(rutaArchivoJson, "utf-8", (error, datos)=>{
-        if(error){
-            return res.json({Error:"no se puede leer los datos."})
-        }
-        const listaAprendices = JSON.parse(datos)
-        res.json(listaAprendices)
-    })
+//middleware body-parsem, formatea los datos enviados
+app.use(express.json())
+//endpoint raiz
+app.get("/", function (req, res) {
+   res.send("API - Rest Aprendices");
 })
-     app.post("/api/aprendices",function (req, res) {
-        res.json({mensaje: "trabajando en el endpoint"})
-     })        
+app.get("/api/aprendices", function (req, res) {
+   //los datos se pueden traer de una base de datos, de un archivo, etc
+   sistemaArchivo.readFile(rutaArchivoJson, "utf8", function(error, datos) {
+      if (error) {
+         return res.json({Error: "No se pudo leer los datos"})
+      }
+      const listaAprendices = JSON.parse(datos);
+      res.json(listaAprendices);
+   })
+})
+//endpoint para crear un aprendiz
+app.post("/api/aprendices", function (req, res) {
+   //validar que se envien los datos
+   const nuevoAprendiz = req.body
+   //utilizamos la lectura del archivo para traer los datos y luego agregar el nuevo aprendiz
+   sistemaArchivo.readFile(rutaArchivoJson, "utf8", function(error, datos) {
+      if (error) {
+         return res.json({Error: "No se pudo leer los datos"})
+      }
+      const listaAprendices = JSON.parse(datos);
+      //agregar el nuevo aprendiz a la lista
+      listaAprendices.push(nuevoAprendiz)
+      //escribir el archivo con la nueva lista
+      sistemaArchivo.writeFile(rutaArchivoJson, JSON.stringify(listaAprendices, null, 
+      2), (error) =>{
+         if (error) {
+            res.status(500).json({Error: "No se pudo escribir los datos"})
+         }
+         res.status(201).json({Mensaje: "Aprendiz creado con exito"})
+      })
+   })
+})
 
-app.listen(puerto, function () {
-     console.log( `SERVIDOR http://localhost:${puerto}`) 
-}); 
+app.listen(PUERTO, function () {
+   console.log(`Servidor http://localhost:${PUERTO}`)
+})
